d8e8ab39c03c37a3d8cf91bc4dcfef4923190065
Matheus Mendes <matheusmendesreis@outlook.com>
2024-10-30 11:20:17 -0300

    new: executando docker compose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import "express-async-errors"
 import { errorHandling } from './middlewares/error-handling'
+import { router } from './routers'
 const app = express()
 app.use(express.json())
-// app.use(errorHandling())
+app.use(errorHandling)
+app.use(router)
 export {app}
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,6 @@
+import {Request,Response} from 'express'
+export class UserController {
+  create(request:Request, response:Response){
+    return response.json({message:"ok"})
+  }
+}
